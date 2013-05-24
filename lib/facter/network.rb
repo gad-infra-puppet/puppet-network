46d9000838ec268f09fb0bb8db2d7f1022322eee
Mingxiang.Zhang <ming.zhang@gree.net>
2013-05-24 11:02:13 +0900

    disable network_public_ip fact

--- lib/facter/network.rb
+++ lib/facter/network.rb
@@ -4,13 +4,13 @@ require 'timeout'
 
 #Public IP
 # Expected output: The public ipaddress of this node.
-Facter.add("network_public_ip") do
-  setcode do
-    Timeout::timeout(2) do
-      open('http://ip-echo.appspot.com', 'User-Agent' => 'Ruby/Facter').read.match(/^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/).to_s
-    end
-  end
-end
+# Facter.add("network_public_ip") do
+#   setcode do
+#     Timeout::timeout(2) do
+#       open('http://ip-echo.appspot.com', 'User-Agent' => 'Ruby/Facter').read.match(/^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/).to_s
+#     end
+#   end
+# end
 
 #Gateway
 # Expected output: The ip address of the nexthop/default router
